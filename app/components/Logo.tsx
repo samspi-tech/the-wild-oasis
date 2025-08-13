78a1ce069de276437f3a7ea349e8c0fbe5ba2f99
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-08-13 23:08:29 +0200

    add navigation between pages

---
+++ app/components/Logo.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
+
+export default function Logo() {
+    return (
+        <Link href="/" className="flex items-center gap-4 z-10">
+            <Image
+                src={logo}
+                width="60"
+                height="60"
+                quality={100}
+                alt="The Wild Oasis logo"
+            />
+            <span className="text-xl font-semibold text-primary-100">
+                The Wild Oasis
+            </span>
+        </Link>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-    return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+    return (
+        <div>
+            <h1>The Wild Oasis. Welcome to paradise.</h1>
+        </div>
+    );
 }
