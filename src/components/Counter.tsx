4eaa5119b15f8922c01f0ca90ad4429c39eb8194
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-08-19 20:44:37 +0200

    removed Users from Counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { useState } from 'react';
-import { type User } from '@/src/app/cabins/page';
 
-type CounterProps = {
-    users: User[];
-};
-
-export default function Counter({ users }: CounterProps) {
+export default function Counter() {
     const [count, setCount] = useState(0);
 
     const handleCount = () => setCount((prevState) => prevState + 1);
 
     return (
         <div>
-            <p>Thera are {users.length} users</p>
             <button onClick={handleCount}>{count}</button>
         </div>
     );
