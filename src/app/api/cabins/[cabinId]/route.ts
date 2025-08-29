77e2d7b9605b87a8425ebe0cd9ec64bd5bf0c55c
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-08-29 23:02:49 +0200

    add custom endpoint for single cabin

---
+++ src/app/api/cabins/[cabinId]/route.ts
@@ -0,0 +1,25 @@
+import { getSingleCabin } from '@/src/lib/dataService/cabin.service';
+import { getBookedDatesByCabinId } from '@/src/lib/dataService/booking.service';
+
+type Params = {
+    params: { cabinId: string };
+};
+
+export async function GET(request: Request, { params }: Params) {
+    const { cabinId } = params;
+    const id = Number(cabinId);
+
+    try {
+        const [cabin, bookedDates] = await Promise.all([
+            getSingleCabin(id),
+            getBookedDatesByCabinId(id),
+        ]);
+
+        return Response.json({
+            cabin,
+            bookedDates,
+        });
+    } catch (error) {
+        return Response.json({ message: 'Cabin not found.' });
+    }
+}
