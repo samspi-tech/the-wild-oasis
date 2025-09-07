d752564a5fe7ea6eac1052d3bee0a00f9a2ae962
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-09-07 12:15:03 +0200

    add custom sign out button

--- src/components/sideNavigation/partials/SignOutButton.tsx
+++ src/components/sideNavigation/partials/SignOutButton.tsx
@@ -1,10 +1,13 @@
+import { signOutAction } from '@/src/lib/actions';
 import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid';
 
 export default function SignOutButton() {
     return (
-        <button className="flex w-full items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100">
-            <ArrowRightOnRectangleIcon className="h-5 w-5 text-primary-600" />
-            <span className="mt-1">Sign out</span>
-        </button>
+        <form action={signOutAction}>
+            <button className="flex w-full items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100">
+                <ArrowRightOnRectangleIcon className="h-5 w-5 text-primary-600" />
+                <span className="mt-1">Sign out</span>
+            </button>
+        </form>
     );
 }
