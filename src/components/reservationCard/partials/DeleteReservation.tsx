9fc0fddf86a0ec1f4f41badf29577f7b426c3e30
samspi-tech <andrew.ryan.r.a@gmail.com>
2025-09-17 21:28:51 +0200

    add loading indicator while deleting reservation

--- src/components/reservationCard/partials/DeleteReservation.tsx
+++ src/components/reservationCard/partials/DeleteReservation.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useTransition } from 'react';
 import { TrashIcon } from '@heroicons/react/24/solid';
 import { deleteReservation } from '@/src/lib/actions';
+import SpinnerMini from '@/src/components/UI/SpinnerMini';
 
 type DeleteReservationProps = {
     bookingId: number;
@@ -10,13 +12,30 @@ type DeleteReservationProps = {
 export default function DeleteReservation({
     bookingId,
 }: DeleteReservationProps) {
+    const [isPending, startTransition] = useTransition();
+
+    const handleDelete = () => {
+        if (confirm('Are you sure you want to delete this reservation?')) {
+            startTransition(() => deleteReservation(bookingId));
+        }
+    };
+
     return (
         <button
-            onClick={() => deleteReservation(bookingId)}
-            className="group flex flex-grow items-center gap-2 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900"
+            disabled={isPending}
+            onClick={handleDelete}
+            className="group flex flex-grow items-center gap-2 px-3 text-xs font-bold uppercase text-primary-300 transition-colors hover:bg-accent-600 hover:text-primary-900 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
         >
-            <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
-            <span className="mt-1">Delete</span>
+            {isPending ? (
+                <span className="mx-auto">
+                    <SpinnerMini />
+                </span>
+            ) : (
+                <>
+                    <TrashIcon className="h-5 w-5 text-primary-600 transition-colors group-hover:text-primary-800" />
+                    <span className="mt-1">Delete</span>
+                </>
+            )}
         </button>
     );
 }
